5506c76884de3bbfb64f66a34f2b625c3a59f5e3
derbur <dburns921@gmail.com>
2021-01-18 13:56:38 -0500

    day 6.2

--- 6/index.ts
+++ 6/index.ts
@@ -2,7 +2,11 @@ import os from 'os';
 import path from "path";
 import { readFile } from "../helpers/FileHelper";
 
-function getAnswerSum(ans: string[]) {
+interface AnswerMap {
+  [key: string]: number
+}
+
+function getUniqueAnswerSum(ans: string[]) {
   return ans.reduce((acc, curr) => {
     // 🤔
     let uniq = new Set([...(curr.split(os.EOL).flat().join('').split(''))]);
@@ -10,9 +14,25 @@ function getAnswerSum(ans: string[]) {
   }, 0);
 }
 
+function getAllAnswerSum(ans: string[]) {
+  return ans.reduce((acc, curr) => {
+    let splitAns = curr.split(os.EOL);
+    let ansMap: AnswerMap = {};
+    splitAns.forEach(answer => {
+      for(let i = 0; i < answer.length; i++) {
+        let n = ansMap[answer.charAt(i)];
+        ansMap[answer.charAt(i)] = n ? ++n : 1;
+      }
+    });
+    // console.log(splitAns, ansMap);
+    return acc + Object.values(ansMap).filter(v => v === splitAns.length).length;
+  }, 0);
+}
+
 function main() {
   let input = readFile(path.join(__dirname, 'input.txt'), os.EOL + os.EOL);
-  console.log(getAnswerSum(input));
+  console.log('UNIQUE ANSWERS:', getUniqueAnswerSum(input));
+  console.log('ALL ANSWERED YES:', getAllAnswerSum(input));
 }
 
 main();
